fa32eb50f2d674fe2c5c49d13b3b386ddc3eaa3d
Tromador <38956076+Tromador@users.noreply.github.com>
2026-09-10 09:51:03 +0100

    port: begin NullPrism Linux support

--- include/Utility/Logging.h
+++ include/Utility/Logging.h
@@ -10,25 +10,29 @@ namespace PS {
     {
         if (optional_arg == RC::LogLevel::Error)
         {
-            auto formatted_log = std::format(STR("[PalSchema] [error] {}"), content);
+            auto formatted_log = RC::File::StringType{STR("[PalSchema] [error] ")};
+            formatted_log.append(content);
             RC::Output::send<optional_arg>(formatted_log, fmt_args...);
         }
         else if (optional_arg == RC::LogLevel::Warning)
         {
-            auto formatted_log = std::format(STR("[PalSchema] [warning] {}"), content);
+            auto formatted_log = RC::File::StringType{STR("[PalSchema] [warning] ")};
+            formatted_log.append(content);
             RC::Output::send<optional_arg>(formatted_log, fmt_args...);
         }
         else if (optional_arg == RC::LogLevel::Verbose)
-        {
+            {
             auto config = PS::PSConfig::Get();
             if (!config->IsDebugLoggingEnabled()) return;
 
-            auto formatted_log = std::format(STR("[PalSchema] [debug] {}"), content);
+            auto formatted_log = RC::File::StringType{STR("[PalSchema] [debug] ")};
+            formatted_log.append(content);
             RC::Output::send<optional_arg>(formatted_log, fmt_args...);
         }
         else
         {
-            auto formatted_log = std::format(STR("[PalSchema] {}"), content);
+            auto formatted_log = RC::File::StringType{STR("[PalSchema] ")};
+            formatted_log.append(content);
             RC::Output::send<optional_arg>(formatted_log, fmt_args...);
         }
     }
